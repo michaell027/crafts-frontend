ede9a4eebec6b851e73ffc78bdcf2fc152104aef
Michaela Majorošová <miselka12345@gmail.com>
2024-03-23 20:42:45 +0100

    fix build failed

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -46,9 +46,9 @@ const LeftSidebar = () => {
             <ul className="space-y-2 font-medium">
               {categories &&
                 categories.map((category: Category) => (
-                  <li key={category.id}>
+                  <li key={category.publicId}>
                     <a
-                      href={`/category/${category.id}`}
+                      href={`/category/${category.publicId}`}
                       className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-400 dark:hover:bg-gray-700 group left-group"
                     >
                       <svg
